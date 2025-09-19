6ce526ae2b80e44600b76faa0b04b764cbc2dc8a
Marius Barbulescu <marius.barbulescu@gmail.com>
2025-09-19 09:58:38 +0200

    Enhance the main functionality by adding file reading capabilities and integrating new questions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,15 @@
 import ai.koog.agents.core.agent.AIAgent
 import ai.koog.agents.core.tools.ToolRegistry
 import ai.koog.agents.ext.tool.SayToUser
+import ai.koog.agents.file.tools.ReadFileTool
 import ai.koog.prompt.executor.clients.openai.OpenAIClientSettings
 import ai.koog.prompt.executor.clients.openai.OpenAILLMClient
 import ai.koog.prompt.executor.clients.openai.OpenAIModels.Chat.GPT4o
 import ai.koog.prompt.executor.llms.SingleLLMPromptExecutor
+import ai.koog.rag.base.files.JVMFileSystemProvider
 import kotlinx.coroutines.runBlocking
 
-fun main() {
+fun main(): Unit = runBlocking {
     val apiKey = requireNotNull(System.getenv("OPENAI_API_KEY")) { "OPENAI_API_KEY is required." }
     val azureOpenAIClientSettings = OpenAIClientSettings(
         baseUrl = "https://isaca-mfmlpbix-northcentralus.openai.azure.com/openai/deployments/gpt-4o-mini-marius/",
@@ -19,16 +21,23 @@ fun main() {
         systemPrompt = "You are a helpful assistant. Answer user questions concisely.",
         llmModel = GPT4o,
         toolRegistry = ToolRegistry {
+            tool(ReadFileTool(JVMFileSystemProvider.ReadOnly))
             tool(SayToUser)
-        }
+        })
+
+
+    val answers = agent.run(
+        """
+            Read only /home/marius/work/github/koog-playground/data/questions.txt file and answer the questions in it.
+            Each line in the file is a question.
+            Say to user answer to these questions.
+            """
     )
-
-    runBlocking {
-        val companyOutput = agent.run("I want to register my company - what steps should I take?")
-        println(companyOutput)
-
-        val documentsOutput = agent.run("What documents I need to open a salary account in Switzerland?")
-        println(documentsOutput)
-    }
+    println("""
+        |---
+        |$answers
+        |---
+    """.trimMargin())
 }
+
 
